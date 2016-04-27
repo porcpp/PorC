1dbfb8da84ea489e3b97c99ad053d2a113d6973c
Fabio Teixeira <fabio1079@gmail.com>
2016-04-27 20:50:13 -0300

    Add unit tests for SimbolTable
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- tests/simbol_table_test.h
+++ tests/simbol_table_test.h
@@ -3,6 +3,20 @@
 
 #include "../lib/simbol_table/simbol_table.h"
 
+void should_create_a_new_simbol_table();
+
+void should_insert_variables();
+
+void should_insert_data();
+
+void should_search_for_a_node();
+
+void should_find_by_a_variable_name();
+
+void should_get_an_array_of_variables();
+
 void execute_all_simbol_table_tests();
+
+static SimbolTable* build_simbol_table_for_tests();
 
 #endif
